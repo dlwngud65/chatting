f9bc4a32e9087c9786804bdf5e19dbce58f0146c
DESKTOP-631SKEV\anywiz <ok@anywiz.co.kr>
2024-07-15 15:15:08 +0900

    커밋합니다.

---
+++ comm/dbcon.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbHost = "112.175.14.15";    // 호스트 주소(localhost, 120.0.0.1)
+$dbName = "appanywiz";      // 데이타 베이스(DataBase) 이름
+$dbUser = "appanywiz";       // DB 아이디
+$dbPass = "appanywiz220429!@";  // DB 패스워드
+$dbChar = "utf8";         // 문자 인코딩
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-
+include_once $_SERVER['DOCUMENT_ROOT']."/include/header.php"; 
+/*
+if(isset($_SESSION['wiz_session']['id'])){
+	//header("location: users.php");
+}
+*/
 ?>
